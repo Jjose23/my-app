59a43617484cb2e7c19a11b43c4e55b54116903d
JuanRodriguez <jjoserodriguez182@gmail.com>
2024-01-31 19:13:39 -0300

    1eraEntrega

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,6 +1,6 @@
 
 function CartWidget(){
-    return(<button type="button" class="btn btn-success position-relative rounded-circle"><ion-icon size="small" name="cart-sharp"></ion-icon>
+    return(<button type="button" class="btn btn-success position-relative rounded-circle"><i class="bi bi-cart"></i>
 
     <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
     3
